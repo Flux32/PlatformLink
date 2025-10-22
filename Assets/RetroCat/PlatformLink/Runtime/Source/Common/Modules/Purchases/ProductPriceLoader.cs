50a576fdffd223c122d8a030b2228a5556681f16
Aleksandr <88793343+Flux32@users.noreply.github.com>
2025-10-23 01:48:57 +0500

    feat: add product price loader

---
+++ Assets/RetroCat/PlatformLink/Runtime/Source/Common/Modules/Purchases/ProductPriceLoader.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using PlatformLink;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace RetroCat.PlatformLink.Runtime.Source.Common.Modules.Purchases
+{
+    public class ProductPriceLoader : MonoBehaviour
+    {
+        [SerializeField] private string _productId;
+        [SerializeField] private bool _loadOnEnable;
+    
+        [SerializeField] private UnityEvent OnLoadStarted;
+        [SerializeField] private UnityEvent<string, Texture2D> OnLoadFinished;
+
+        private Coroutine _coroutine;
+    
+        private void OnEnable()
+        {
+            if (_loadOnEnable)
+            {
+                if (_coroutine != null)
+                    return;
+            
+                StartCoroutine(LoadProductPrice());
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (_coroutine != null)
+            {
+                StopCoroutine(_coroutine);
+                _coroutine = null;
+            }
+        }
+
+        private IEnumerator LoadProductPrice()
+        {
+            OnLoadStarted?.Invoke();
+
+#if UNITY_EDITOR
+            yield return new WaitForSeconds(1f);
+#endif
+            
+            bool isLoaded = false;
+            CatalogProduct loadedProduct = null;
+        
+            PLink.Purchases.GetProduct(_productId, (success, product) =>
+            {
+                isLoaded = success;
+                loadedProduct = product;
+            });
+        
+            yield return new WaitUntil(() => isLoaded && loadedProduct != null);
+        
+            isLoaded = false;
+            Texture2D loadedCurrencyIcon = null;
+        
+            loadedProduct.PriceCurrencyIcon.LoadTexture((success, currencyIcon ) =>
+            {
+                isLoaded = true;
+                loadedCurrencyIcon = currencyIcon;
+            });
+        
+            yield return new WaitUntil(() => isLoaded && loadedCurrencyIcon != null);
+            OnLoadFinished?.Invoke(loadedProduct.PriceValue, loadedCurrencyIcon);
+        }
+    }
+}
