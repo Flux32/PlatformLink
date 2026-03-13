c9ec037fa65de0e8ded7a573aee5b820862a4a5e
Aleksandr Skobenko <88793343+Flux32@users.noreply.github.com>
2026-03-13 05:13:42 +0500

    feat: disable web visor

--- Assets/RetroCat/PlatformLink/Runtime/Source/Common/Modules/Analytics/Analytics.cs
+++ Assets/RetroCat/PlatformLink/Runtime/Source/Common/Modules/Analytics/Analytics.cs
@@ -150,7 +150,7 @@ $@"{MetrikaStartMarker}
         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
     }})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id={counterId}', 'ym');
 
-    ym({counterId}, 'init', {{ssr:true, webvisor:true, clickmap:true, ecommerce:""dataLayer"", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true}});
+    ym({counterId}, 'init', {{ssr:true, webvisor:false, clickmap:true, ecommerce:""dataLayer"", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true}});
 </script>
 <noscript><div><img src=""https://mc.yandex.ru/watch/{counterId}"" style=""position:absolute; left:-9999px;"" alt="""" /></div></noscript>
 {MetrikaEndMarker}";
